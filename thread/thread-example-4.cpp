f6125517aed95fcd533bd58993457fddc1287b70
Kaoru <shiinasama2001@gmail.com>
2024-03-31 10:52:06 +0800

    feat: 补充 async 使用示例

--- thread/thread-example-4.cpp
+++ thread/thread-example-4.cpp
@@ -1,6 +1,26 @@
 #include "sese/util/Initializer.h"
+#include "sese/util/Util.h"
+#include "sese/thread/Async.h"
+#include "sese/record/Marco.h"
 
 int main(int argc, char **argv) {
     sese::initCore(argc, argv);
+
+    SESE_INFO("begin");
+    {
+        auto future = sese::async<std::string>([]() -> std::string {
+            sese::sleep(1s);
+            return "Hello, World";
+        });
+        SESE_INFO("result: %s", future.get().c_str());
+    }
+    {
+        auto future = sese::asyncWithGlobalPool<std::string>([]() -> std::string{
+            sese::sleep(1s);
+            return "Bye";
+        });
+        SESE_INFO("result: %s", future.get().c_str());
+    }
+
     return 0;
 }
